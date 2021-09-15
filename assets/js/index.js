cf74393e973815a6cae6d1b508f087f542aabc01
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-09-15 17:33:05 -0500

    Style like button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,13 @@
-console.log('Hola mundo');
+const $likeButton = document.querySelector('#like');
+const $likeCount = document.querySelector('#likeCount');
+
+let isLiked = false;
+
+$likeButton.addEventListener('click', () => {
+    $likeButton.classList.toggle('is-liked');
+    isLiked = !isLiked;
+
+    isLiked
+        ? ($likeCount.textContent = parseInt($likeCount.textContent) + 1)
+        : ($likeCount.textContent = parseInt($likeCount.textContent) - 1);
+});
